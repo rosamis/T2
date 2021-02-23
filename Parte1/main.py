4c6753b78f2f5350ac692aae6aac0a5d222d1e36
Roberta <ro.samis.comp@gmail.com>
2021-02-23 17:33:13 -0300

    Parte 1 ok

--- Parte1/main.py
+++ Parte1/main.py
@@ -1,10 +1,56 @@
 import xmltodict
 import json
+import sys
+from os import listdir
+from os.path import isfile, join
 
-# xml_content = open('dataset/finance/AndroidManifest_albert_3.2.1-3210.xml')
+def listFiles(path):
+    return [str(f) for f in listdir(path) if isfile(join(path, f))]
 
+def permissionAPK(list_files, name_dir):
+    print('===================')
+    print('Permissões por APK')
+    print('===================')
 
-with open('dataset/finance/AndroidManifest_albert_3.2.1-3210.xml') as fd:
-    doc = xmltodict.parse(fd.read())
+    total = {}
+    for lf in list_files:
+        list_permissions = []
+        with open(name_dir + '/' + lf) as fd:
+            doc = xmltodict.parse(fd.read())
+            for d in doc["manifest"]["uses-permission"]:
+                for key, value in d.items(): 
+                    if 'permission.' in value:
+                        if '.' in value.split('permission.')[1]:
+                            list_permissions.append(value.split('permission.')[1].split('.')[0])
+                        else:
+                            list_permissions.append(value.split('permission.')[1])
+            file_name = lf.split('AndroidManifest_')[1].replace('.xml','')
+            print(file_name,': ', list_permissions)
+        total[file_name] = list_permissions
 
-print(doc["manifest"]["uses-permission"])
+    print('===================')
+    print('Permissões únicas por APK')
+    print('===================')
+
+    unique_permission = {}
+    for key in total.keys():
+        temp_dic = total.copy()
+        temp_dic.pop(key)
+        unique_permission[key] = list(set(total[key]).difference(*temp_dic.values()))
+        print(key + ': ', unique_permission[key])
+
+    print('===================')
+    print('Permissões comuns das APKs')
+    print('===================')
+
+    values_dic = list(total.values())
+    common = set(values_dic[0]).intersection(*values_dic[1:])
+    print(list(common))
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        name_dir = sys.argv[1]
+        list_files = listFiles(name_dir)
+        permissionAPK(list_files, name_dir)
+    else:
+        print('Error: missing args')
